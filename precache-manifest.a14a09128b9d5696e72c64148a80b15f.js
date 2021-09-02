eb99b94ff667c39e137efd136979608a9d0360de
Siyu Yue <siyu@verishop.com>
2021-09-02 11:57:05 -0700

    Initial pages

---
+++ precache-manifest.a14a09128b9d5696e72c64148a80b15f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1312bf96032181fd56f87d00ca06a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c07e84f64b3d9c4184",
+    "url": "/static/js/2.3f4d5ab7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3f4d5ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be02c573f93ebf3fb9a5",
+    "url": "/static/js/main.0307fec9.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
